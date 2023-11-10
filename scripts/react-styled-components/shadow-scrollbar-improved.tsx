9ed0a72f8387b35a2bde8f66a640b7e0a4da101b
Daniel da Silva <mailpatudo@gmail.com>
2023-11-10 14:52:40 +0000

    Create shadow-scrollbar-improved.tsx

---
+++ scripts/react-styled-components/shadow-scrollbar-improved.tsx
@@ -0,0 +1,44 @@
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { ShadowScrollbar } from '@devseed-ui/shadow-scrollbar';
+
+export const ShadowScrollbarImproved = React.forwardRef<
+  HTMLDivElement,
+  React.ComponentProps<typeof ShadowScrollbar>
+>(function ShadowScrollbarImprovedCmp(props, ref) {
+  // eslint-disable-next-line react/prop-types
+  const { scrollbarsProps, ...rest } = props;
+  const [autoHide, setAutoHide] = useState(true);
+
+  const mouseEnter = useCallback(() => setAutoHide(false), []);
+
+  useEffect(() => {
+    setAutoHide(false);
+  }, []);
+
+  useEffect(() => {
+    if (autoHide) return;
+
+    const timer = setTimeout(() => {
+      setAutoHide(true);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [autoHide]);
+
+  const shadowScrollbarProps = useMemo(
+    () => ({
+      ...(scrollbarsProps ?? {}),
+      autoHide
+    }),
+    [scrollbarsProps, autoHide]
+  );
+
+  return (
+    <ShadowScrollbar
+      ref={ref}
+      onMouseEnter={mouseEnter}
+      scrollbarsProps={shadowScrollbarProps}
+      {...rest}
+    />
+  );
+});
